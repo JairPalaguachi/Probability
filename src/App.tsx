285b8730af763a6a0873c487a7d03df172b44585
SKEIILAT <javimacias619@gmail.com>
2025-10-04 14:50:40 -0500

    Componentes de checkbox y elegir fecha

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import LocationPickerMap from './components/LocationPickerMap';
 import InfoPanel from './components/InfoPanel';
-
-
-
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { CheckBox, CheckRounded } from '@mui/icons-material';
+import FormControlLabel from '@mui/material/FormControlLabel'; 
+import FormGroup from '@mui/material/FormGroup'
+import CheckBoxFuncional from './components/CheckBoxFuncional';
 function App() {
   return (
     <div className="app-grid">
@@ -12,7 +16,16 @@ function App() {
       <div className="app-grid-right">
         <LocationPickerMap />
       </div>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div>
+        <p>Fecha del Evento</p>
+        <DatePicker label="Basic date picker" />
+        <p>Condiciones a analizar</p>
+        <CheckBoxFuncional/>
+      </div>
+    </LocalizationProvider>
     </div>
+    
   );
 }
 
