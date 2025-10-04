c492b15559309748297fb584ff3d88dcc76ea73e
SKEIILAT <javimacias619@gmail.com>
2025-10-04 16:33:40 -0500

    Se agregó cambio del picker de la hora

--- src/components/FechaPicker.tsx
+++ src/components/FechaPicker.tsx
@@ -2,13 +2,13 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TextField } from '@mui/material';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
 export function FechaPicker() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div>
-        <DatePicker label="Elige la Fecha" />
-        
+        <DateTimePicker label="Elige la Fecha" />
       </div>
     </LocalizationProvider>
   );
